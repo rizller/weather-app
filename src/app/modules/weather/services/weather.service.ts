abe6059401011c2cbdd9968bc40e1ac92f62705b
Rizller Souza <rizller.souza@prodemge.gov.br>
2024-05-14 20:00:28 -0300

    test

--- src/app/modules/weather/services/weather.service.ts
+++ src/app/modules/weather/services/weather.service.ts
@@ -6,6 +6,7 @@ import { Observable } from 'rxjs';
 @Injectable({ //o injectable diz que essa classe pode ser injetada em um componente
   providedIn: 'root' //o provideIn, diz aonde será provida. Como está como root, qualquer componente independente do módulo tem acesso à esse serviço.
   //Mas caso queria que esteja disponivel somente em um módulo específico, colocar na aba de providers, segmentando em apenas um módulo.
+
 })
 export class WeatherService {
   private apiKey = "93987142748b67853f19bb9905f10d11";
